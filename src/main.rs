cd13c56c840075ccc844cfdb38704c1eb879a553
kunarkkhewal <kunark.khewal88@gmail.com>
2024-01-02 22:44:40 +0530

    added ferris_says crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use ferris_says::say;
+use std::io::{BufWriter, stdout};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let message = String::from("Hello World!!!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
