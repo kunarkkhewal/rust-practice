0008ba0940ef2864fa482e4efc34a7cead074116
kunarkkhewal <kunark.khewal88@gmail.com>
2024-02-13 19:14:27 +0530

    Iterators

--- src/main.rs
+++ src/main.rs
@@ -670,23 +670,120 @@ fn main() {
     // );
 
     // Referencing
-    let mut list = vec![1, 2, 3];
-    println!("Before defining closure: {:?}", list);
+    // let mut list = vec![1, 2, 3];
+    // println!("Before defining closure: {:?}", list);
 
-    let mut only_borrows = || list.push(7);//println!("From closure: {:?}", list);
+    // let mut only_borrows = || list.push(7);//println!("From closure: {:?}", list);
 
-    // println!("Before calling closure: {:?}", list);
-    // list.push(4);
-    only_borrows();
-    println!("After calling closure: {:?}", list);
+    // // println!("Before calling closure: {:?}", list);
+    // // list.push(4);
+    // only_borrows();
+    // println!("After calling closure: {:?}", list);
 
 
-    let list = vec![1, 2, 3];
-    println!("Before defining closure: {:?}", list);
+    // let list = vec![1, 2, 3];
+    // println!("Before defining closure: {:?}", list);
 
-    thread::spawn(move || println!("From thread: {:?}", list))
-        .join()
-        .unwrap();
+    // thread::spawn(move || println!("From thread: {:?}", list))
+    //     .join()
+    //     .unwrap();
+
+    // ITERATOR
+    let v1 = vec![1, 2, 3];
+
+    // let v1_iter = v1.iter();
+
+    for val in v1 {
+        println!("Got: {}", val);
+    }
+
+    pub trait Iterator {
+        type Item;
+    
+        fn next(&mut self) -> Option<Self::Item>;
+    
+        // methods with default implementations elided
+    }
+
+    let v1: Vec<i32> = vec![1, 2, 3];
+
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    println!("v2: {:?}", v2);
+    for val in v1 {
+        println!("Got v2: {}", val);
+    }
+}
+
+#[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1, 2, 3];
+
+        let mut v1_iter = v1.iter();
+
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+    }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+
+        let v1_iter = v1.iter();
+
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+    }
+
+    #[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
 }
 
 // #[derive(Debug, PartialEq, Copy, Clone)]
